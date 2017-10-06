8c7278182fbba19c1ab94bddb99224d72fc9a9ed
mt-taro <tm.pos97@gmail.com>
2017-10-06 12:15:03 +0900

    main.rbを更新しました

--- main.rb
+++ main.rb
@@ -3,32 +3,82 @@ require 'dxruby'
 
 require_relative 'player'
 require_relative 'enemy'
+require_relative 'block'
+require_relative 'bullet'
 
-Window.width  = 800
-Window.height = 600
+Window.width  = 640
+Window.height = 480
 
 player_img = Image.load("player.png")
 player_img.setColorKey([0, 0, 0])
 
-enemy_img = Image.load("enemy.png")
-enemy_img.setColorKey([0, 0, 0])
+bullet_img = Image.load("bullet.png")
+bullet_img.setColorKey([0, 0, 0])
 
-player = Player.new(400, 500, player_img)
+start_img = Image.load("start.png")
+start_img.setColorKey([0, 0, 0])
 
+end_img = Image.load("end.png")
+end_img.setColorKey([0, 0, 0])
+
+player = Player.new(288, 448, player_img)
+block = Block.new
 enemies = []
-10.times do
-  enemies << Enemy.new(rand(800), rand(600), enemy_img)
-end
+bullets = []
+
+count = 1
+random = Random.new
+
+st = 0
+over = 0
 
 Window.loop do
+  if st == 0
+    Window.loop do
+      Window.draw(0, 0, start_img)
+      if Input.keyPush?(K_SPACE)
+        st = 1
+        break
+      end
+      break if Input.keyPush?(K_ESCAPE)
+    end
+  end
+  
   break if Input.keyPush?(K_ESCAPE)
-
+  
+  count -= 1
+  if count == 0
+    count = 110
+    a = random.rand(0..2)
+    enemies.push(block.generate(a))
+    enemies.flatten!
+  end
+  
+  if Input.keyPush?(K_Z)
+    bullets = Bullet.new(player.x+8, player.y, bullet_img)
+  end
+  
   Sprite.update(enemies)
   Sprite.draw(enemies)
-
+  Sprite.update(bullets)
+  Sprite.draw(bullets)
   player.update
   player.draw
 
   # 当たり判定
-  Sprite.check(player, enemies)
+  Sprite.check(enemies, enemies)
+  Sprite.check(enemies, player)
+  Sprite.check(bullets, enemies)
+  
+  if player.over() == 1
+    Window.loop do
+      Window.draw(0, 0, end_img)
+      if Input.keyPush?(K_ESCAPE)
+        over = 1
+        break
+      end
+    end
+  end
+  break if over == 1
+
 end
