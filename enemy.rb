9c2fe167b0b13a7cb72e33699407e7b44e17d368
mt-taro <tm.pos97@gmail.com>
2017-10-06 12:14:09 +0900

    enemy.rbを更新しました

--- enemy.rb
+++ enemy.rb
@@ -1,15 +1,19 @@
 # coding: utf-8
-
+require_relative 'bullet'
 class Enemy < Sprite
   def update
-    self.y += 1
-    if self.y >= Window.height - self.image.height
-      self.vanish
+    if (self.y < Window.height - self.image.height) && (@flag == nil)
+      self.y += 1
     end
   end
 
   # 他のオブジェクトから衝突された際に呼ばれるメソッド
   def hit(obj)
-    self.vanish
+    if obj.kind_of?(Enemy)
+      @flag = 1
+    end
+    if obj.kind_of?(Bullet)
+      self.vanish
+    end
   end
 end
